864c8e67ba8de2ee1512cfe9825d24e1049542d8
PEDRO HENRIQUE BITTENCOURT GOMES <0049972@IFMG.EDU.BR>
2023-03-07 16:09:55 -0300

    Tela de cadastro

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,102 +1,212 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,TextInput, ImageBackground,TouchableOpacity} from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, ImageBackground, Alert, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 
 export default function Login() {
-  return (
-    <ImageBackground 
+  const [step, setStep] = useState(0);
+
+  const [usuario, setUsuario] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [repeatpassword, setRepeatPassword] = useState('');
+
+
+  function handleSubmit() {
+    console.log({ usuario, password });
+    //alert('Usuário:' + usuario + 'Senha:' + password);
+  }
+  function changeForm() {
+    if (step === 0) {
+      setStep(1);
+    } else {
+      setStep(0);
+    }
+  }
+
+  function validateForm() {
+    if (name === '') {
+      Alert.alert('Informe seu nome');
+      return;
+    }
+    if (usuario === '') {
+      Alert.alert('Preencha o campo usuario');
+      return;
+    }
+    if (password === '') {
+      Alert.alert('Preencha o campo senha');
+      return;
+    }
+    if (repeatpassword === '') {
+      Alert.alert('Repita sua senha');
+      return;
+    }
+    if (password !== repeatpassword) {
+      Alert.alert('As senhas nao conferem');
+      return;
+    }
+
+
+  }
+
+return (
+  <ImageBackground
     style={styles.container}
-        source ={{
-            uri : 
-            'https://i.pinimg.com/564x/ab/6a/12/ab6a126748031b76fbfadae55da925f1.jpg'
+    source={{
+      uri:
+        'https://i.pinimg.com/564x/ab/6a/12/ab6a126748031b76fbfadae55da925f1.jpg'
 
-        }}
-        resizeMode ="contain"
->
-      <Text style = {styles.title}> Bem-Vindo</Text>
-      
-      <View style = {styles.form}>
-      <Text style={styles.label}>Usuário</Text>
-      
-      <TextInput
-      style={styles.input}
-      placeholder= "Digite seu usuário"
-      keyboardAppearance='e-mail-address'
+    }}
+    resizeMode="strech"
+  >
+    <Text style={styles.title}>UAIPELOTA</Text>
 
-/>
-<Text style={styles.label}>Senha</Text>
-      
-      <TextInput
-      style={styles.input}
-      placeholder= "Sua senha secreta"
-       textSecurityEntry={true}
+    {step == 0 ? (
+      <KeyboardAvoidingView style={styles.form} behavior="padding">
+
+        <Text style={styles.label}>Usuário</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder=" Digite seu usuário"
+          keyboardAppearance='e-mail-address'
+          value={usuario}
+          onChangeText={setUsuario}
+
+        />
+        <Text style={styles.label}>Senha</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder=" Sua senha secreta"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
 
 
-/>
-<TouchableOpacity style={styles.button}>
-    <Text style={styles.buttonText}>Entrar
-    </Text>
-</TouchableOpacity>
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Entrar
+          </Text>
+        </TouchableOpacity>
 
-<TouchableOpacity>
-<Text style = {[styles.label,{textAlign:'center'}]}>Cadastre-se grátis!</Text>
-</TouchableOpacity>
+        <TouchableOpacity onPress={changeForm}>
+          <Text style={[styles.label, { textAlign: 'center' }]}>Cadastre-se grátis!</Text>
+        </TouchableOpacity>
 
-   </View>
-</ImageBackground>
-   
-  
-  );
+      </KeyboardAvoidingView>
+    ) : (
+      <View style={[styles.form, {marginTop:170}]}>
+        <Text style={styles.label}>Nome</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder=" Seu nome completo"
+          keyboardAppearance='e-mail-address'
+          value={name}
+          onChangeText={setName}
+
+        />
+        <Text style={styles.label}>Usuário</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder=" Digite seu usuário"
+          keyboardAppearance='e-mail-address'
+          value={usuario}
+          onChangeText={setUsuario}
+
+        />
+        <Text style={styles.label}>Senha</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder=" Sua senha secreta"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+
+
+        />
+        <Text style={styles.label}>Repita sua senha</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder=" Sua senha secreta"
+          secureTextEntry={true}
+          value={repeatpassword}
+          onChangeText={setRepeatPassword}
+
+
+        />
+        <TouchableOpacity style={styles.button} onPress={validateForm}>
+          <Text style={styles.buttonText}>Cadastrar
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={changeForm}>
+          <Text style={[styles.label, { textAlign: 'center' }]}>Ja possuo conta!</Text>
+        </TouchableOpacity>
+
+      </View>
+    )}
+
+  </ImageBackground>
+
+
+);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
+
     backgroundColor: '#000',
+    alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  title: {
+    fontSize: 36,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginBottom: 30,
+    marginTop: 50,
+
+  },
+  form: {
+    marginTop: 270,
+    width: '90%',
+    alignSelf: 'center',
+
+
+  },
+  label: {
+
+    color: '#fff',
+    fontSize: 16,
+    marginBottom: 12,
+    marginTop: 20,
+    marginLeft: 10,
+  },
+  input: {
+    backgroundColor: '#fff',
+    width: '100%',
+    height: 40,
+    borderRadius: 20,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#028039',
+    height: 40,
+    borderRadius: 20,
+    marginTop: 30,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize : 36,
+  buttonText: {
     color: '#fff',
-    fontWeight:'bold',
-    marginBottom:30,
-
-  },
-  form:{
-    marginTop:30,
-    width:'90%',
-    
- 
- },
-  label:{
-
-    color: '#fff',
-    fontSize:16,
-    marginBottom:12,
-    marginTop:20,
-    marginLeft:10,
-  },
-  input:{
-    backgroundColor:'#fff',
-    width:'100%',
-    height: 40,
-    borderRadius:20,
-    fontWeight:'bold',
-  },
-  button:{
-    backgroundColor:'#028039',
-    height:40,
-    borderRadius:20,
-    marginTop:30,
-    alignItems:'center',
-    justifyContent:'center',
-  },
-  buttonText:{
-    color:'#fff',
-    fontSize:18,
-    textTransform:'uppercase',
-    fontWeight:'bold'
+    fontSize: 18,
+    textTransform: 'uppercase',
+    fontWeight: 'bold'
 
   }
 
